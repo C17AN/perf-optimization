36dd5e1045b9bf2789e9257607c74e9ac4fb5774
김찬민 <chanmin@gimchanmin-ui-MacBookPro.local>
2022-11-05 02:03:05 +0900

    인터섹션 옵저버 기반 지연 로딩

---
+++ src/components/Card.js
@@ -0,0 +1,32 @@
+import React, { useEffect, useRef } from "react";
+
+function Card(props) {
+  const imageRef = useRef(null);
+
+  const callback = (entries, observer) => {
+    entries.forEach((entry) => {
+      console.log(entry.isIntersecting, entry.target.dataset.src);
+      entry.target.src = entry.target.dataset.src;
+      observer.unobserve(entry.target);
+    });
+  };
+
+  const observer = new IntersectionObserver(callback, {});
+  useEffect(() => {
+    observer.observe(imageRef.current);
+    return () => {
+      observer.disconnect();
+    };
+  }, []);
+
+  return (
+    <div className="Card text-center">
+      <img data-src={props.image} ref={imageRef} />
+      <div className="p-5 font-semibold text-gray-700 text-xl md:text-lg lg:text-xl keep-all">
+        {props.children}
+      </div>
+    </div>
+  );
+}
+
+export default Card;
